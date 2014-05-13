a85d7b3190cbba97f47013c51dd1a4c10b5842ea
Trenton Maki <trentonmaki@gmail.com>
2014-05-13 11:41:31 -0700

    Initial commit

---
+++ Frankenstein/fraMyScene.h
@@ -0,0 +1,13 @@
+//
+//  fraMyScene.h
+//  Frankenstein
+//
+
+//  Copyright (c) 2014 Trenton Maki. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface fraMyScene : SKScene
+
+@end
