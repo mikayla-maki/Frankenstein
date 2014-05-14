ed14d7a38702dccd2cbdb801c3e7d6f322c040be
Trenton Maki <trentonmaki@gmail.com>
2014-05-14 15:53:41 -0700

    Initial Commit

---
+++ Frankenstein2/fraViewController.h
@@ -0,0 +1,14 @@
+//
+//  fraViewController.h
+//  Frankenstein2
+//
+
+//  Copyright (c) 2014 Trenton Maki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface fraViewController : UIViewController
+
+@end
