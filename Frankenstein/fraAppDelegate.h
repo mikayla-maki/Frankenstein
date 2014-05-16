07d6a9271e54e654525944fcdcc6d7722eef24a2
Trenton Maki <trentonmaki@gmail.com>
2014-05-15 21:17:24 -0700

    Trying to clean everything up

---
+++ Frankenstein/fraAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  fraAppDelegate.h
+//  Frankenstein
+//
+//  Created by Trenton Maki on 5/15/14.
+//  Copyright (c) 2014 Trenton Maki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fraAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Frankenstein/fraViewController.m
@@ -0,0 +1,51 @@
+//
+//  fraViewController.m
+//  Frankenstein
+//
+//  Created by Trenton Maki on 5/15/14.
+//  Copyright (c) 2014 Trenton Maki. All rights reserved.
+//
+
+#import "fraViewController.h"
+#import "fraMyScene.h"
+
+@implementation fraViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [fraMyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+@end
