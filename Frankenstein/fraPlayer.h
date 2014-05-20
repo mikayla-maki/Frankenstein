9c2ca441d90e5734d0fd4290a767a765f2638c37
Trenton Maki <trentonmaki@gmail.com>
2014-05-20 14:42:26 -0700

    Working on player

---
+++ Frankenstein/fraPlayer.h
@@ -0,0 +1,13 @@
+//
+//  fraPlayer.h
+//  Frankenstein
+//
+//  Created by Trenton Maki on 5/20/14.
+//  Copyright (c) 2014 Trenton Maki. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface fraPlayer : SKSpriteNode
+
+@end
---
+++ Frankenstein/fraPlayer.m
@@ -0,0 +1,13 @@
+//
+//  fraPlayer.m
+//  Frankenstein
+//
+//  Created by Trenton Maki on 5/20/14.
+//  Copyright (c) 2014 Trenton Maki. All rights reserved.
+//
+
+#import "fraPlayer.h"
+
+@implementation fraPlayer
+    
+@end
